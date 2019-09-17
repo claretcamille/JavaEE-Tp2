895f31118537f28fb77b9e724688a8b9f95d40d7
camilleclaret <camille.claret@etud.univ-jfc.fr>
2019-09-17 18:21:04 +0200

    Ajout des tests Unitaires

--- src/test/java/ticketmachine/TicketMachineTest.java
+++ src/test/java/ticketmachine/TicketMachineTest.java
@@ -34,30 +34,62 @@ public class TicketMachineTest {
                         //S3: Non impression du ticket si le montant est insufissant
                         public void notPrintTicket(){
 
-                                                   
+                                                 // Insertion prix necessaire
+                                                machine.insertMoney(30); // Ici on peut utiliser Price - nbr avec nbr >= 1 pour etre sur que si on change la valeur price le test continu a changer
+                                                // Verification que les montants sont differents :
+                                               assertNotSame( machine.getBalance(),machine.getPrice());
+                                               //Test de la non impression du ticket : 
+                                               assetFalse(machine.printTicket());  
                         }
                         
                         @Test
                         //S4: Impression du ticket si le montant est sufissant
                         public void PrintTicket(){
-                                                
+                                                 // Insertion prix necessaire => val PRICE
+                                                machine.insertMoney(PRICE);
+                                                // Verification que les montants sont differents :
+                                               assertNotSame( machine.getBalance(),machine.getPrice());
+                                               //Test de la non impression du ticket : 
+                                               assetTrue(machine.printTicket());
                         }
                         
                         @Test
                         //S5: Décrémentation prix balance
                         public void PrintTicketAndBalance(){
-                                                
+                            
+                                                // Insertion d'un montant supperrieur 
+                                                machine.insertMoney(PRICE+1);
+                                                // Impression du ticket
+                                                machine.printTicket();
+                                                // Vérification de la décrementation 
+                                                assertEquals(1,machine.getBalance()); // Il reste 1 euro
                         }
                         
                         @Test
                         //S6: Mise a jour du montant collecter lors de l'impression du ticket
                         public void updatePrice(){
+                            
+                                                // Mise en place d'un ticket
+                                                machine.insertMoney(PRICE);
+                                                machine.printTicket();
+                                                // Vérification du montant collecter
+                                                assertEquals(PRICE, machine.getTotal());
+                                                
+                                                // Si plusieur montant collecter
+                                                machine.printTicket();
+                                                machine.printTicket();
+                                                assertEquals(PRICE*3, machine.getTotal()); // 3 tickets imprimer ==> le total est de 3 fois le prix du ticket 
                                                 
                         }
                         
                         @Test
                         //S7: Rendu monnaie -> est-il correct ?
                         public void renduMonnaie(){
+                                                // Si machine vide on rend 0
+                                                assertEquals(0, machine.refund());
+                                                //  veriffication rendu si machine non vide
+                                                machine.insertMoney(PRICE);
+                                                assertEquals(PRICE, machine.refund());
                                                 
                         }
                         
@@ -65,21 +97,33 @@ public class TicketMachineTest {
                         //S8: remise a zéro de la balance
                         public void initializeBalance(){
                                                 
+                                                // Insertion de la monnaie -> on est sur que la balance est donc différente de 0
+                                                machine.insertMoney(PRICE);
+                                                // Remise a zéro de la balance 
+                                                 machine.refund();
+                                                 assertEquals(0, machine.getBalance());
                         }
                         
                         @Test
                         //S9: Pas de montant négatif dans la machine 
                         public void notNegatif(){
+                                               try{
+                                                             // Insertion montant négatif:
+                                                            machine.insertMoney(-1);
+                                               } catch (IllegalArgumentException e) {
+                                                            assertEquals(0,machine.getBalance()); // Aucun ajout n'est fait dans la machine ==> 0
+                                               }
                                                 
                         }
                         
                         @Test
                         //S10: Pas de machine dont le ticket est négatif
                         public void notNegatifTicket(){
-                                                
+                                                try{
+                                                           machine =  new TicketMachine(-1);
+                                                } catch (IllegalArgumentException e) {
+                                                             assertEquals(PRICE,machine.getPrice());  // On vérifie que la machine n'a pa pu changer
+                                               }
                         }
 
-    private void assetFalse(boolean printTicket) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
 }
