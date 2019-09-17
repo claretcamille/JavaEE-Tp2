185de96e213da5e96adb0cc8c787277cf0edfee2
camilleclaret <camille.claret@etud.univ-jfc.fr>
2019-09-17 17:44:39 +0200

    Preparation du TP

--- src/test/java/ticketmachine/TicketMachineTest.java
+++ src/test/java/ticketmachine/TicketMachineTest.java
@@ -29,5 +29,57 @@ public class TicketMachineTest {
 		machine.insertMoney(20);
 		assertEquals("La balance n'est pas correctement mise à jour", 10 + 20, machine.getBalance()); // Les montants ont été correctement additionnés               
 	}
+        
+                        @Test
+                        //S3: Non impression du ticket si le montant est insufissant
+                        public void notPrintTicket(){
 
+                                                   
+                        }
+                        
+                        @Test
+                        //S4: Impression du ticket si le montant est sufissant
+                        public void PrintTicket(){
+                                                
+                        }
+                        
+                        @Test
+                        //S5: Décrémentation prix balance
+                        public void PrintTicketAndBalance(){
+                                                
+                        }
+                        
+                        @Test
+                        //S6: Mise a jour du montant collecter lors de l'impression du ticket
+                        public void updatePrice(){
+                                                
+                        }
+                        
+                        @Test
+                        //S7: Rendu monnaie -> est-il correct ?
+                        public void renduMonnaie(){
+                                                
+                        }
+                        
+                        @Test
+                        //S8: remise a zéro de la balance
+                        public void initializeBalance(){
+                                                
+                        }
+                        
+                        @Test
+                        //S9: Pas de montant négatif dans la machine 
+                        public void notNegatif(){
+                                                
+                        }
+                        
+                        @Test
+                        //S10: Pas de machine dont le ticket est négatif
+                        public void notNegatifTicket(){
+                                                
+                        }
+
+    private void assetFalse(boolean printTicket) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
 }
